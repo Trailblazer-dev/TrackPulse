2941efee4c1bc55f1f83c50840a6c2e594784ced
Trailblazer_dev <richvictor830@gmail.com>
2025-08-08 20:34:49 +0300

    rolling up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
 import { useTheme } from './hooks/useTheme';
 import ThemeTransitionOverlay from './components/common/ThemeTransitionOverlay';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const { theme, isTransitioning } = useTheme();
@@ -11,6 +12,7 @@ function App() {
       <div className="min-h-screen ">
         <AppRoutes />
         <ThemeTransitionOverlay isVisible={isTransitioning} theme={theme} />
+        <Analytics />
       </div>
     </BrowserRouter>
   );
